74bb71fc027f921a79fa3e2bffbbc43a49d9c382
Ikhbayar Sukhbaatar <23LP4139@G303-CC0137.local>
2024-02-07 09:06:52 +0800

    Committing changes to frontend/src/app/Home/page.tsx before merging

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -1,30 +1,7 @@
-'use client'
-// Home component
-import * as React from 'react';
-import { Box } from '@mui/material';
-import Navbar from "../../components/Navbar";
-import Modal from '@/components/Modal';
-import Login from '@/components/Login';
-import Footer from '@/components/Footer';
-import YourComponent from '@/components/Test';
-import HomePage from '@/components/Home';
+import Home from "../page";
 
-export default function Home() {
-  const [modal, setModal] = React.useState(false);
-
-  const handleModal = () => {
-    setModal(!modal);
-    console.log(!modal);
-    console.log('first');
-  }
-
-  return (
-    <Box height="fit-content">
-      <Navbar onClick={handleModal} />
-      {modal && <Modal />}
-      <HomePage/>
-      {/* <SignUp/> */}
-      {/* <Footer/> */}
-    </Box>
-  );
+export default function sectionHome() {
+    return (
+        <Home/>
+    )
 }
