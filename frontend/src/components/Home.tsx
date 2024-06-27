e0876b632dde6dc1b99f0e26a6267e73164e728b
Bayra1 <isvhbaatar5@gmail.com>
2024-06-27 10:52:02 +0800

    test

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -10,26 +10,62 @@ import SaleFoods from "./SaleFoods";
 import { BasketCard } from "./BasketCardModal";
 
 export default function HomePage() {
-    return (
-        <Box width={"100%"}>
-            <Box sx={{ display: "flex" }} alignItems='center' flexDirection='column' bgcolor='#18BA51' justifyContent='center' height='788px'>                            
-                <Box justifyContent='space-between' width={"80%"} alignItems='center' sx={{ display: "flex" }}>
-                    <Stack>
-                        <Typography sx={{ fontWeight: 500 }} color='white' variant="h4">PineCone <br /> Food Delivery</Typography>
-                        <Typography mb={2} mt={2} border={"1px solid white"}></Typography>
-                        <Typography color='white' variant="h6">Horem ipsum dolor sit amet,</Typography>
-                        <Typography color='white' variant="h6">consectetur adipiscing elit.</Typography>
-                    </Stack>
-                    <Stack sx={{ width: "450xp", height: "450xp" }}>
-                        <img className="w-[450px] h-p[450px]" src={BigFood.src} alt="" />
-                    </Stack>
-                </Box>
-            </Box>
-            <Deliveries />
-            <SaleFoods />
-            <MainFood />
-            <Footer />
-            {/* <BasketCard/> */}
+  return (
+    <Box
+      width={"100%"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          backgroundImage:
+            "url('https://s3-alpha-sig.figma.com/img/abab/8928/c4c79b15138c71b44dc8c2202844dd61?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VTsdD-i2y132X5eGVs7agHzMVkLzUXPpknSaDo0IT4z9q5ku2SpC47LqOgYfq~mnQS5TIkzTMxFYFetrAZW0pq0gbCqYeofXzsgMEceW2tus8QFbY2IOWlZm8gUNKWiuBNZTcx3W1kYyoXwv9pZs9yfcIPSi7lSwEkE1OS8ood8gn~~0W2NYAa97jzUEsLW3ZYT0eToWnberOhVASmtV2AXhbWSAs0wGjswGEi2Qcp7YZuxBTbsfPWNjlizrIQDbLtJZ2u2bx9CwtsljoLlLByKIBqgULAiNlO~cZwdNxdlkb5wcY3UCF~pWDVcwCAhK2S-rvRqvNXB2u0uOEkYGog__')",
+          backgroundSize: "cover", 
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          width: "1200px",
+          height: "500px",
+        }}
+        alignItems="center"
+        flexDirection="column"
+        bgcolor="#18BA51"
+        justifyContent="center"
+        height="788px"
+      >
+        <Box
+          justifyContent="space-between"
+          width={"80%"}
+          alignItems="center"
+          sx={{
+            display: "flex",
+            // Prevents the image from repeating
+          }}
+        >
+          <Stack>
+            <Typography sx={{ fontWeight: 500 }} color="white" variant="h4">
+              PineCone <br /> Food Delivery
+            </Typography>
+            <Typography mb={2} mt={2} border={"1px solid white"}></Typography>
+            <Typography color="white" variant="h6">
+              Horem ipsum dolor sit amet,
+            </Typography>
+            <Typography color="white" variant="h6">
+              consectetur adipiscing elit.
+            </Typography>
+          </Stack>
+          <Stack sx={{ width: "450px", height: "450px" }}>
+            <img className="w-[450px] h-p[450px]" src={BigFood.src} alt="" />
+          </Stack>
         </Box>
-    );
-};
+      </Box>
+      <Deliveries />
+      <SaleFoods />
+      <MainFood />
+      <Footer />
+      {/* <BasketCard/> */}
+    </Box>
+  );
+}
